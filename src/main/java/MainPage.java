23fdf4f0234d07bbd78590644615672666acab2c
AndreasFulop <fulopand@freemail.hu>
2021-08-05 11:48:28 +0200

    screenshots (first try)

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,10 +1,10 @@
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +20,7 @@ public class MainPage {
     private final By TITLE_CHECKS = By.xpath("//*[@id=\"content\"]");
 
     WebDriver driver;
+
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
@@ -27,17 +28,17 @@ public class MainPage {
     public List<String> tableRows() {
         List<WebElement> rows = driver.findElements(TABLE_ROWS);
         List<String> result = new ArrayList<>();
-        for (WebElement i:rows) {
+        for (WebElement i : rows) {
             String text = i.findElement(By.xpath("./td[1]")).getText();
             result.add(text);
         }
-            return result;
+        return result;
     }
 
     public List<String> tableSecRows() {
         List<WebElement> rows = driver.findElements(TABLE_ROWS2);
         List<String> result = new ArrayList<>();
-        for (WebElement i:rows) {
+        for (WebElement i : rows) {
             String text = i.findElement(By.xpath("./td[2]")).getText();
             result.add(text);
         }
@@ -47,7 +48,7 @@ public class MainPage {
     public List<String> tableNameRows() {
         List<WebElement> rows = driver.findElements(TABLE_HEAD);
         List<String> result = new ArrayList<>();
-        for (WebElement i:rows) {
+        for (WebElement i : rows) {
             String text = i.findElement(By.xpath("./th")).getText();
             result.add(text);
         }
@@ -57,18 +58,25 @@ public class MainPage {
     public List<String> tableColNames() {
         List<WebElement> rows = driver.findElements(TABLE_NAMES);
         List<String> result = new ArrayList<>();
-        for (WebElement i:rows) {
+        for (WebElement i : rows) {
             String text = i.getText();
             result.add(text);
         }
         return result;
     }
 
-    public String[] checksRadios(){
+    public String[] checksRadios() {
         String[] result = new String[2];
-        click(FORMS_MENU);
-        click(CHECKS);
-
+        try {
+            click(FORMS_MENU);
+        } catch (Exception e) {
+            Allure.addAttachment("First click image", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        }
+        try {
+            click(CHECKS);
+        } catch (Exception e) {
+            Allure.addAttachment("Second click image", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        }
         result[0] = driver.findElement(TITLE_CHECKS).getText();
         result[1] = driver.getCurrentUrl();
 
